50bdea9d5b3d2c23aca6429b890769efe29db8e7
heyaadarsh <aadarsh0001.be21@chitkara@edu.in>
2022-05-25 13:45:53 +0530

    Added functions directory in the repository | After being added as a collaborator with write role!

---
+++ C++ Programs/Functions/sumfunc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void sum(){
+    int a;
+    int b;
+    cin>>a, b;
+    int sumVal = a + b;
+    // return sumVal;
+};
+
+int main(){
+    sum();
+    return 0;
+}
